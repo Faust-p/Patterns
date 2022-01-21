c6f7773934ab253585a58682c7cef3414acee9cd
Alexander Pavlenko <82059804+Faust-p@users.noreply.github.com>
2022-01-21 16:17:22 +0200

    Add files via upload

---
+++ task5/ChocolateBoiler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        private static ChocolateBoiler uniqueInstance;
+        private static object LockThis = new object();
+
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (LockThis)
+                {
+                    if (uniqueInstance == null)
+                    {
+                        uniqueInstance = new ChocolateBoiler();
+                    }
+                }
+            }
+            return uniqueInstance;
+        }
+
+        public void fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = false;
+                Console.WriteLine("Fill the boiler");
+            }
+        }
+
+        public void drain()
+        {
+            if (!empty && boiled)
+            {
+                empty = true;
+                Console.WriteLine("Drain");
+            }
+        }
+
+        public void boil()
+        {
+            if (!empty && !boiled)
+            {
+                boiled = true;
+                Console.WriteLine("Boil");
+            }
+        }
+    }
+}
---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.GetInstance();
+            boiler.fill();
+            boiler.boil();
+            boiler.drain();
+            Console.ReadKey();
+        }
+    }
+}
